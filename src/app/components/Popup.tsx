0914412b0457da041af953a932774949d1638169
nepaldynasty09 <nepaldynasty08@gmail.com>
2024-09-25 12:46:28 +0545

    api calls

--- src/app/components/Popup.tsx
+++ src/app/components/Popup.tsx
@@ -1,32 +1,45 @@
-
-
 import React from 'react';
+import Image from 'next/image';
 
 interface PopupProps {
     status: string;
-
 }
 
 const Popup: React.FC<PopupProps> = ({ status }) => {
     let message = '';
     let bgColor = '';
+    let imageSrc = '';
 
-    if (status === 'success') {
+    if (status === 'Checked In!') {
         message = 'Ticket is valid!';
         bgColor = 'bg-green-500';
-    } else if (status === 'fail') {
+        imageSrc = '/assets/images/accepted.svg';
+    } else if (status === 'Security code is not valid!') {
         message = 'Ticket is invalid!';
         bgColor = 'bg-red-500';
-    } else if (status === 'duplicate') {
+        imageSrc = '/assets/images/failed.svg';
+    } else if (status === 'Already checked in!') {
         message = 'Ticket is already checked!';
         bgColor = 'bg-yellow-500';
+        imageSrc = '/assets/images/alreadycheckedin.svg';
     }
 
     return (
         <div className="fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-black bg-opacity-50 z-50">
-            <div className={`p-14 rounded-xl max-w-3xl w-full text-center shadow-lg text-white ${bgColor}`}>
-                <h2 className="text-4xl font-bold mb-6">{message}</h2>
-
+            <div
+                className={`w-96 h-96 flex flex-col justify-center items-center p-8 shadow-2xl ${bgColor} rounded-lg border-2 border-white`}>
+                {imageSrc && (
+                    <Image
+                        src={imageSrc}
+                        alt={status}
+                        width={100}
+                        height={100}
+                        className="mb-4"
+                    />
+                )}
+                <h2 className="text-4xl font-bold text-white text-center">
+                    {message}
+                </h2>
             </div>
         </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import Popup from './components/Popup';
 
 export default function Home() {
@@ -7,24 +7,62 @@ export default function Home() {
   const [ticketId, setTicketId] = useState<string | null>(null);
   const [securityKey, setSecurityKey] = useState<string | null>(null);
   const [popupStatus, setPopupStatus] = useState<string | null>(null);
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+
+  const url = 'https://tixort.au/?event_qr_code=1&ticket_id=10614&event_id=10601&security_code=69b3ab0bd3&path=wp-json%2Ftribe%2Ftickets%2Fv1%2Fqr';
 
 
-  const handleSubmit = async () => {
-    //popup check garna fake api call 
-    // params: success,fail,duplicate
-    const res = await new Promise((resolve) =>
-      setTimeout(() => resolve('success'), 1000)
-    );
-    setPopupStatus(res as string);
-  };
+  useEffect(() => {
+    const parsedUrl = new URL(url);
+    const params = new URLSearchParams(parsedUrl.search);
+
+    const extractedEventId = params.get('event_id');
+    const extractedTicketId = params.get('ticket_id');
+    const extractedSecurityKey = params.get('security_code');
+
+    const delay = setTimeout(() => {
+      setEventId(extractedEventId);
+      setTicketId(extractedTicketId);
+      setSecurityKey(extractedSecurityKey);
+    }, 2000);
+
+    return () => clearTimeout(delay);
+  }, [url]);
+
+
+  const handleSubmit = useCallback(async () => {
+    const apiUrl = `/api/v1/tickets/qr?ticket_id=${ticketId}&event_id=${eventId}&security_code=${securityKey}&api_key=${apiKey}`;
+
+    try {
+      const response = await fetch(apiUrl);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(`Message: ${data.msg}`);
+        setPopupStatus(data.msg);
+      } else {
+        const data = await response.json();
+        setPopupStatus(data.msg)
+      }
+    } catch (error) {
+      console.error('Error fetching the API:', error);
+    }
+  }, [ticketId, eventId, securityKey, apiKey]);
+
+
+  useEffect(() => {
+    if (eventId && ticketId && securityKey) {
+      handleSubmit();
+    }
+  }, [eventId, ticketId, securityKey, handleSubmit]);
 
   const handleNextScan = () => {
-
     setEventId(null);
     setTicketId(null);
     setSecurityKey(null);
     setPopupStatus(null);
+
   };
+
 
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
@@ -35,31 +73,17 @@ export default function Home() {
     };
 
     document.addEventListener('keydown', handleKeyDown);
-
-
-    return () => {
-      document.removeEventListener('keydown', handleKeyDown);
-    };
+    return () => document.removeEventListener('keydown', handleKeyDown);
   }, []);
 
 
   useEffect(() => {
-    if (eventId && ticketId && securityKey) {
-      handleSubmit();
-    }
-  }, [eventId, ticketId, securityKey]);
-
-
-  const closePopup = () => {
-    setPopupStatus(null);
-  };
-
-  useEffect(() => {
     if (popupStatus) {
-      const timer = setTimeout(() => closePopup(), 3000);
+      const timer = setTimeout(() => setPopupStatus(null), 3000);
       return () => clearTimeout(timer);
     }
   }, [popupStatus]);
+
 
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-50 p-6">
@@ -111,7 +135,10 @@ export default function Home() {
         </button>
       </form>
 
+      {/* Display popup if available */}
       {popupStatus && <Popup status={popupStatus} />}
+
+
     </div>
   );
 }
